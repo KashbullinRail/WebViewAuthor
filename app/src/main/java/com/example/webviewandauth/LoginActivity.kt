a97b2415c2b2f80d4baffa143b4ba64bc07997ea
KashbullinRail <kashbullinrail@gmail.com>
2022-11-13 17:07:44 +0300

    отложенная инициализация переменных

--- app/src/main/java/com/example/webviewandauth/LoginActivity.kt
+++ app/src/main/java/com/example/webviewandauth/LoginActivity.kt
@@ -1,11 +1,39 @@
 package com.example.webviewandauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private val etEmail: EditText by lazy { findViewById(R.id.etEmail) }
+    private val etPassword: EditText by lazy { findViewById(R.id.etPassword) }
+    private val btnLogin: Button by lazy { findViewById(R.id.btnLogin) }
+    private val btnCreateAccount: Button by lazy { findViewById(R.id.btnCreateAccount) }
+
+    private val mAuth:FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            if (etEmail.text.toString().isEmpty() || etPassword.text.toString().isEmpty()){
+                Toast.makeText(this, "email or password field is empty", Toast.LENGTH_LONG).show()
+            } else{
+                mAuth.signInWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/webviewandauth/RegistrationActivity.kt
+++ app/src/main/java/com/example/webviewandauth/RegistrationActivity.kt
@@ -2,8 +2,15 @@ package com.example.webviewandauth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private val etEmailReg:EditText by lazy { findViewById(R.id.etEmailReg) }
+    private val etPasswordReg:EditText by lazy { findViewById(R.id.etPasswordReg) }
+    private val btnRegistration:Button by lazy { findViewById(R.id.btnRegistration) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
