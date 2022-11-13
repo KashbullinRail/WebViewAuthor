dcb45494673df8550033edd1710f4546600ddf77
KashbullinRail <kashbullinrail@gmail.com>
2022-11-13 14:53:04 +0300

    добавлен WebView, привязана логика открытия ссылок в предидущем коммите добавлен permission для internet

--- app/src/main/java/com/example/webviewandauth/Constants.kt
+++ app/src/main/java/com/example/webviewandauth/Constants.kt
@@ -3,3 +3,4 @@ package com.example.webviewandauth
 const val GOOGLE_URL = "https://www.google.com"
 const val YouTube_URL = "https://www.youtube.com"
 const val StackOverFlow_URL = "https://stackoverflow.com"
+const val GITHUB_URL = "https://github.com"
--- app/src/main/java/com/example/webviewandauth/WebViewActivity.kt
+++ app/src/main/java/com/example/webviewandauth/WebViewActivity.kt
@@ -1,7 +1,10 @@
 package com.example.webviewandauth
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,6 +14,7 @@ class WebViewActivity : AppCompatActivity() {
 
     private val navView: NavigationView by lazy { findViewById(R.id.navView) }
     private val drawer:DrawerLayout by lazy { findViewById(R.id.drawer) }
+    private val wvWeb:WebView by lazy { findViewById(R.id.wvWeb) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +25,19 @@ class WebViewActivity : AppCompatActivity() {
         navView.setNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.google -> {
+                    webViewStart(GOOGLE_URL)
                     toast("Google")
                 }
                 R.id.youtube -> {
+                    webViewStart(YouTube_URL)
                     toast("YouTube")
                 }
                 R.id.stackoverflow -> {
+                    webViewStart(StackOverFlow_URL)
                     toast("StackOverFlow")
                 }
                 R.id.github -> {
+                    webViewStart(GITHUB_URL)
                     toast("Github")
                 }
             }
@@ -41,6 +49,21 @@ class WebViewActivity : AppCompatActivity() {
 
     fun toast(text:String){
         Toast.makeText(this@WebViewActivity, "Загружается сайт $text", Toast.LENGTH_SHORT).show()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    fun webViewStart(url: String) {
+        wvWeb.webViewClient = WebViewClient()
+
+        wvWeb.apply {
+            loadUrl(url)
+            settings.javaScriptEnabled = true
+            settings.loadWithOverviewMode = true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (wvWeb.canGoBack()) wvWeb.goBack() else super.onBackPressed()
     }
 
 }
